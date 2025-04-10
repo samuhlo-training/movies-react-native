85077123ae2cc687c638a91c106040a15df8ee2e
samuhlo <samu13lop@gmail.com>
2025-04-10 14:42:27 +0200

    nativewind config + navbar + structure

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MoviesDetails = () => {
+  return (
+    <View>
+      <Text>MoviesDetails</Text>
+    </View>
+  )
+}
+
+export default MoviesDetails
+
+const styles = StyleSheet.create({})
