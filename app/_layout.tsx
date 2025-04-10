ead8a133f95c61064d6566461abbb266b3bd61b4
samuhlo <samu13lop@gmail.com>
2025-04-10 18:10:44 +0200

    fetch movie details function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
-import './globals.css';
+import "./globals.css";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack >
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-    <Stack.Screen name="movies/[id]" options={{ headerShown: false }}/>
-  </Stack>;
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-const MoviesDetails = () => {
+const MovieDetail = () => {
   return (
     <View>
-      <Text>MoviesDetails</Text>
+      <Text>MovieDetail</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MoviesDetails
+export default MovieDetail;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
